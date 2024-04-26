85c8c60a43573f4a75af5b353095327fc4b74309
Nicolas Lebacq <nicolas.cordier@outlook.com>
2024-04-26 10:14:14 +0200

    feat: Added final working MQTT broker image.

--- mqtt-to-sqs/files/entrypoint.sh
+++ mqtt-to-sqs/files/entrypoint.sh
@@ -1,64 +1,50 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Checking environment..."
+function log() {
+	local date_format="%Y-%m-%d_%T"
+	local current_date="[$(date +${date_format})]"
+
+	if [[ "$#" -eq 0 ]]; then
+
+		echo "${current_date}: No argument to print."
+
+	else
+
+		echo "${current_date}: $@"
+
+	fi
+}
+
+log "Checking environment..."
 
 NOT_SET=false
 
-if ! [[ -v AWS_ACCESS_KEY_ID ]]; then
-  echo "AWS_ACCESS_KEY_ID is not set"
-  NOT_SET=true
-fi
-if ! [[ -v AWS_SECRET_ACCESS_KEY ]]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set"
-  NOT_SET=true
-fi
-if ! [[ -v AWS_REGION ]]; then
-  echo "AWS_REGION is not set"
-  NOT_SET=true
-fi
-if ! [[ -v AWS_ACCOUNT_ID ]]; then
-  echo "AWS_ACCOUNT_ID is not set"
-  NOT_SET=true
-fi
-if ! [[ -v AWS_SQS_QUEUE_NAME ]]; then
-  echo "AWS_SQS_QUEUE_NAME is not set"
-  NOT_SET=true
-fi
 if ! [[ -v MQTT_USERNAME ]]; then
-  echo "MQTT_USERNAME is not set"
+  log "MQTT_USERNAME is not set"
   NOT_SET=true
 fi
+
 if ! [[ -v MQTT_PASSWORD ]]; then
-  echo "MQTT_PASSWORD is not set"
+  log "MQTT_PASSWORD is not set"
   NOT_SET=true
 fi
 
 if [ "$NOT_SET" = true ]; then
-  echo "Some environment variables are not set. Exiting..."
+  log "Some environment variables are not set. Exiting..."
   exit 1
 fi
 
-
-echo "Configuring AWS CLI"
-
-aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}"
-aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}"
-aws configure set region "${AWS_REGION}"
-aws configure set output json
-
-echo "AWS CLI Configured"
-
-echo "Creating mosquitto password file"
+log "Creating mosquitto password file"
 echo "${MQTT_USERNAME}:${MQTT_PASSWORD}" > /home/mosquitto/configuration/passwd
-echo "Created mosquitto password file"
+log "Created mosquitto password file"
 
-echo "Registering MQTT passwd"
+log "Registering MQTT passwd"
 mosquitto_passwd -U /home/mosquitto/configuration/passwd
-echo "MQTT passwd registered"
+log "MQTT passwd registered"
 
 mkdir -p /home/mosquitto/logs
 touch /home/mosquitto/logs/mosquitto.log
 
 mosquitto -d -c /home/mosquitto/configuration/mosquitto.conf
-{ tail -f /home/mosquitto/logs/mosquitto.log & mosquitto_sub -h localhost -u "${MQTT_USERNAME}" -P "${MQTT_PASSWORD}" -t "#" -F "%j" | /home/mosquitto/scripts/send_to_sqs_queue.sh; }
+{ tail -f /home/mosquitto/logs/mosquitto.log & node /home/node/app/build/app.mjs; }
--- mqtt-to-sqs/files/send_to_sqs_queue.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-while read line; do
-  echo "Received message: ${line}"
-  echo "Sending message to SQS on region ${AWS_REGION}, account ${AWS_ACCOUNT_ID} and queue ${AWS_SQS_QUEUE_NAME}"
-  aws sqs send-message --queue-url "https://sqs.${AWS_REGION}.amazonaws.com/${AWS_ACCOUNT_ID}/${AWS_SQS_QUEUE_NAME}" --message-body "${line}"
-  echo "Message sent to SQS"
-done
